26df790166db0874464dce9c6683498f414a1a84
harsha <saiharshapingali@gmail.com>
2023-12-03 18:21:08 +0530

    Created API To Fetch Chats

--- controllers/chatControllers.js
+++ controllers/chatControllers.js
@@ -48,3 +48,23 @@ export const accessChat = asyncHandler(async (req, res) => {
     throw new Error(error.message);
   }
 });
+
+export const fetchChats = asyncHandler(async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(201).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,11 +1,11 @@
 import express from "express";
 import { protect } from "../middlewares/authMiddleware.js";
-import { accessChat } from "../controllers/chatControllers.js";
+import { accessChat, fetchChats } from "../controllers/chatControllers.js";
 
 const router = express.Router();
 
 router.route("/").post(protect, accessChat); // for accessing or creating
-// router.route("/").get(protect, frtchChats);
+router.route("/").get(protect, fetchChats);
 // router.route("/group").post(protect, createGroupChats);
 // router.route("/rename").put(protect, renameGroup);
 // router.route("/groupremove").put(protect.removeFromGroup);
